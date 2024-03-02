cd09e8f425979445f9d313f022e7d12ea671e96e
mefodei <mefodei.link@gmail.com>
2024-03-02 17:38:23 +0700

    - add multiscene reference for addressable

---
+++ Runtime/AssetReferenceMultiScene.cs
@@ -0,0 +1,11 @@
+﻿namespace UniGame.MultiScene.Runtime
+{
+    using System;
+    using UnityEngine.AddressableAssets;
+
+    [Serializable]
+    public class AssetReferenceMultiScene : AssetReferenceT<MultiSceneAsset>
+    {
+        public AssetReferenceMultiScene(string guid) : base(guid) {}
+    }
+}
